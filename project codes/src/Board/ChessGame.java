81118aa295a720731de2a169838721219bb2401d
Mostafaali3 <mostafaalisarhan@gmail.com>
2023-05-03 04:45:08 +0300

    0

--- project codes/src/Board/ChessGame.java
+++ project codes/src/Board/ChessGame.java
@@ -1,6 +1,8 @@
 package Board;
 
 import Pieces.Army;
+import Pieces.PieceColor;
+import Pieces.WhitePawn;
 
 public class ChessGame {
     private final GameBoard board;
@@ -9,6 +11,8 @@ public class ChessGame {
         Army whiteArmy=new Army();
         Army blackArmy=new Army();
         board = new GameBoard(whiteArmy,blackArmy);
-        
+        for (int c=0;c<GameBoard.LENGTH;c++){
+            whiteArmy.addPiece(new WhitePawn(board.getSquare(1,c), PieceColor.WHITE));
+        }
     }
 }
